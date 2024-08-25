8d5f0f8753871b650764a8d29f2215a19b3a1556
gwatsin <gwatsinthong@gmail.com>
2024-08-25 16:26:52 +0530

    Variables

--- home.php
+++ home.php
@@ -7,10 +7,25 @@
 </head>
 <body>
     <h1>My first PHP page</h1>
-    
+
     <?php
         echo "Hello World!";
+        // comment
+        # comment
+        /*
+        multi line comment
+        */
     ?>
+    <?php
+        $x = 5;
+        $y = "John";
 
+        echo "$y is $x years old.";
+
+        $z = 5;
+        var_dump($z);
+
+        $a = $b = $c = 10;
+    ?>
 </body>
 </html>
