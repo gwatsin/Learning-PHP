5a99d1e9dcc71d1fa806bf16f5bb34241e6e0be2
gwatsin <gwatsinthong@gmail.com>
2024-08-26 16:40:05 +0530

    Some learnings

--- home.php
+++ home.php
@@ -26,6 +26,26 @@
         var_dump($z);
 
         $a = $b = $c = 10;
+
+        function myTest(){
+            echo "<p>Variable x inside function is: $x</p>";
+        }
+        myTest();
+
+        echo "<p>Variable x outside function is: $x</p>";
+    ?>
+
+    <?php
+        $x = 5;
+        $y = 10;
+
+        function myTest2(){
+            global $x, $y;
+            $y = $x + $y;
+        }
+
+        myTest2();
+        echo $y;
     ?>
 </body>
 </html>
